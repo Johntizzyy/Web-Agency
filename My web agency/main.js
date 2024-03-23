f62b931babb2781b777e14daba1c6ca383047d5d
Johntizzyy <100945217+Johntizzyy@users.noreply.github.com>
2024-03-23 14:02:49 +0100

    Add files via upload

---
+++ My web agency/main.js
@@ -0,0 +1,13 @@
+const john = document.getElementsByClassName('John')
+const tiz = document.getElementsByClassName('tiz')
+const logo = document.getElementsByClassName('logo')
+const img = document.querySelectorAll('img')
+
+logo.addEventListener('click',() => {
+    tiz.style.color= '#fff'
+    alert('opor')
+})
+
+img.addEventListener('click',() => {
+    alert('opor fun johntizzy agba coder')
+})
